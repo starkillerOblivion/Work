c379d4004c2645bb7cf86312c4476798020ebc9e
Student48 <satrker20202@gmail.com>
2025-03-17 18:16:22 +0300

    Добавьте файлы проекта.

---
+++ Pages/AuthorizationPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Work.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationPage.xaml
+    /// </summary>
+    public partial class AuthorizationPage : Page
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAuthor_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new CapchaPage());
+        }
+
+        private void MouseLeave(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void MouseEnter(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Pages/CapchaPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Work.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для CapchaPage.xaml
+    /// </summary>
+    public partial class CapchaPage : Page
+    {
+        Random rnd=new Random();
+        public string capcha = "";
+        public CapchaPage()
+        {
+            InitializeComponent();
+           UpdateCapcha();
+        }
+        private void GenerateNoise(int volumeNoise)
+        {
+            for(int i=0;i< volumeNoise;i++)
+            {
+                Rectangle rectangle = new Rectangle();
+                rectangle.Fill = new SolidColorBrush(Color.FromArgb((byte)rnd.Next(35,120),(byte)rnd.Next(256), (byte)rnd.Next(256), (byte)rnd.Next(256)));
+                rectangle.Width = rectangle.Height = rnd.Next(3, 90);
+                cv.Children.Add(rectangle);
+                Canvas.SetLeft(rectangle, rnd.Next(0, 350));
+                Canvas.SetTop(rectangle, rnd.Next(0, 250));
+            }
+        }
+        private void GenerateSymbols(int count)
+        {
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            for(int i=0;i<count;i++)
+            {
+                string symbol=alphabet.ElementAt(rnd.Next(0,alphabet.Length)).ToString();
+                TextBlock textBlock = new TextBlock();
+                textBlock.Text = symbol;
+                textBlock.FontSize = rnd.Next(10, 84);
+                textBlock.RenderTransform = new RotateTransform(rnd.Next(-45, 45));
+                textBlock.Margin=new Thickness(10,10,10,10);
+                textBlock.Foreground = new SolidColorBrush(Color.FromArgb((byte)rnd.Next(200, 256), (byte)rnd.Next(200, 256), (byte)rnd.Next(200, 256), (byte)rnd.Next(200, 256)));
+                SpCapch.Children.Add(textBlock);
+                capcha += symbol;
+            }
+        }
+        private void UpdateCapcha()
+        {
+            SpCapch.Children.Clear();
+            cv.Children.Clear();
+            GenerateSymbols(5);
+            GenerateNoise(10);
+        }
+        private void UpdCapch_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateCapcha();
+        }
+
+        private void CheckCapch_Click(object sender, RoutedEventArgs e)
+        {
+            if(capcha==tbCapch.Text.Trim().ToUpper())
+            {
+                NavigationService.Navigate(new MainPage());
+                MessageBox.Show("я не бот)");
+            }
+            else
+            {
+                MessageBox.Show("я бот(");
+            }
+        }
+    }
+}
